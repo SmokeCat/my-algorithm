1f21515a712f65bec3d04393307a3a9154ce74b0
smokecat <smokecatyo@gmail.com>
2019-10-11 20:16:13 +0800

    update

--- leetcode/AlgorithmsCode/src/test/java/com/smoke/solution/Solution39Test.java
+++ leetcode/AlgorithmsCode/src/test/java/com/smoke/solution/Solution39Test.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class Solution39Test {
 	static Solution39 s39;
@@ -18,7 +21,7 @@ class Solution39Test {
 	private static List<List<Integer>> res;
 
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	static void setUpBeforeClass() throws Exception { 
 		s39 = new Solution39();
 		res = new ArrayList<List<Integer>>();
 		
@@ -29,8 +32,16 @@ class Solution39Test {
 //		res.add(new ArrayList<Integer>(Arrays.asList(3,5)));
 	}
 
-	@Test
-	void test() {
+	@ParameterizedTest
+	@CsvSource({
+		"'[2,3,6,7]', 7, '[[7], [2, 2, 3]]'"
+	})
+//	@Test
+	void test(
+//			@ConvertWith() int[]candidates,
+//			int target,
+//			@ConvertWith(String2int.class) int[][] res
+			) {
 		List<List<Integer>> currRes = s39.combinationSum(candidates, target);
 		assertTrue(currRes.containsAll(res) && res.containsAll(currRes), "solution39 failed");
 	}
---
+++ leetcode/AlgorithmsCode/src/test/java/com/smoke/util/To2DArrayListArgumentConverter.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.smoke.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+
+/**
+ *	用于Junit5 @ConvertWith 的类型转换器
+ * 
+ * @author Smoke
+ *
+ */
+public class To2DArrayListArgumentConverter extends SimpleArgumentConverter {
+
+	
+	/**
+	 * convert int[][] string to List<List<Integer>>
+	 */
+	@Override
+	public List<List<Integer>> convert(Object input, Class<?> targetClass) throws ArgumentConversionException {
+		if (!String.class.equals(targetClass)){
+			throw new ArgumentConversionException("Cannot convert to " + targetClass.getName() + ": " + input);
+		}
+		
+		return Arrays.stream(((String)input).split("],\\["))
+			      .map(row -> row.replace("[[", "").replace("]]", ""))
+			      .map(row -> Arrays.stream(row.split(","))
+			        .map(Integer::parseInt).collect(Collectors.toList())
+			      ).collect(Collectors.toList());
+	}
+//	
+//	/**
+//	 *	调试用
+//	 * 
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+////		int arg[][] = {{7}, {2,2,3}};
+////		
+////		for(int[] arg1 : arg){
+////			for(int i : arg1){
+////				System.out.println(arg1.toString());
+////			}
+////		}
+//		
+//		String s = "[[7],[2,2,3]]";
+//
+//		List<List<Integer>> res = new ArrayList<List<Integer>>();
+//		
+//		res.add(new ArrayList<Integer>(Arrays.asList(7)));
+//		res.add(new ArrayList<Integer>(Arrays.asList(2,2,3)));
+//		
+//		System.out.println(Arrays.stream(s.split("],\\["))
+//			      .map(row -> row.replace("[[", "").replace("]]", ""))
+//			      .map(row -> Arrays.stream(row.split(","))
+//			        .map(Integer::parseInt).collect(Collectors.toList())
+//			      ).collect(Collectors.toList()));
+//	}
+
+}
