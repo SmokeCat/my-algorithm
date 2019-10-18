91f2dbbafd55c64d2c2285958fe5a51542a6a88d
smokecat <smokecatyo@gmail.com>
2019-10-18 15:52:38 +0800

    add problem 88 code and test

--- leetcode/AlgorithmsCode/src/main/java/com/smoke/solution/Solution88.java
+++ leetcode/AlgorithmsCode/src/main/java/com/smoke/solution/Solution88.java
@@ -8,5 +8,13 @@ package com.smoke.solution;
  *
  */
 public class Solution88 {
-
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = m - 1, j = n - 1, ptr = nums1.length - 1; ptr >= 0;){
+            if(j < 0 || i >= 0 && nums1[i] >= nums2[j]){
+                nums1[ptr--] = nums1[i--];
+            }else{
+                nums1[ptr--] = nums2[j--];
+            }
+        }
+    }
 }
