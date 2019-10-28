7b1d649aeb4cca2e17e4f013ff4fe820a0abecdd
smokecat <smokecatyo@gmail.com>
2019-10-28 20:55:09 +0800

    update Converter

--- leetcode/AlgorithmsCode/src/test/java/com/smoke/util/converter/To2DIntArrayArguementConverter.java
+++ leetcode/AlgorithmsCode/src/test/java/com/smoke/util/converter/To2DIntArrayArguementConverter.java
@@ -3,6 +3,8 @@
  */
 package com.smoke.util.converter;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 
@@ -13,9 +15,39 @@ import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 public class To2DIntArrayArguementConverter extends SimpleArgumentConverter {
 
 	@Override
-	protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-		// TODO Auto-generated method stub
-		return null;
+	protected Object convert(Object input, Class<?> targetClass) throws ArgumentConversionException {
+		input = ((String) input).replace(" ", "");
+		if (!checkSource((String)input)){
+			if(isEmptyArray((String)input)){
+				return new int[0][];
+			}
+			throw new ArgumentConversionException("candidates: \"" + input + "\" cannot convert to " + targetClass.getName() + ": ");
+		}
+
+		return Stream.of(((String) input).replace("[[", "").replace("]]", "").split("],\\["))
+				.map(row -> Stream.of(row.split(","))
+					.mapToInt(Integer::parseInt).toArray()
+					)
+				.toArray(size -> new int[size][]);
 	}
 
+	/**
+	 *	检查输入的字符串是否能够转化为二维数组
+	 * 
+	 * @param source
+	 * @return
+	 */
+	private boolean checkSource(String source){
+		return source.matches("^\\[(\\[((-?[1-9]\\d*|0),?)*\\],?)+\\]$") ;
+	}	
+	
+	/**
+	 *	检查输入的字符串是否是一个空数组
+	 * 
+	 * @param source
+	 * @return
+	 */
+	private boolean isEmptyArray(String source){
+		return source.matches("^\\[\\]$");
+	}
 }
