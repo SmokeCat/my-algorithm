bf71b4044bafc65d46ff7c93e4160fcbd73d0f78
smokecat <smokecatyo@gmail.com>
2019-11-01 10:19:36 +0800

    add optimized code

--- leetcode/AlgorithmsCode/src/main/java/com/smoke/solution/Solution62.java
+++ leetcode/AlgorithmsCode/src/main/java/com/smoke/solution/Solution62.java
@@ -19,4 +19,24 @@ public class Solution62 {
     	
 		return paths[m-1][n-1];
     }
+    
+    // 优化空间复杂度为O(n)
+    public int uniquePaths2(int m, int n) {
+    	if(m == 0 || n == 0) return 0;
+    	else if(n == 1 || m == 1) return 1;
+        
+        int[] paths = new int[n];
+        
+        for(int i=0; i < n; i++){
+            paths[i] = 1;
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                paths[j] += paths[j-1];
+            }
+        }
+    	
+		return paths[n-1];
+    }
 }
